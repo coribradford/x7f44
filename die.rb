8df85413389839a795a9ce17aa0aedd561bcd8a3
Cori Bradford <bradfcor@oregonstate.edu>
2021-12-08 11:30:25 -0800

    remove random object and references, fixes #02

--- die.rb
+++ die.rb
@@ -2,16 +2,14 @@ class Die
 
   attr_reader :number_of_sides
   attr_accessor :value
-  attr_reader :prng
 
   def initialize(number_of_sides)
     @number_of_sides = number_of_sides
     @value = 1
-    @prng = Random.new
   end
 
   def roll!
-    self.value = prng.rand(1..number_of_sides)
+    self.value = rand(1..number_of_sides)
   end
 
   def makeRoll
